2e88807660221fc84dca58554bbbac09b16cdb60
Argen-Develo <cuentapeke27@gmail.com>
2023-06-14 20:16:39 -0300

    comienzo de descripciones del proyecto

--- src/componets/about/About.jsx
+++ src/componets/about/About.jsx
@@ -4,7 +4,12 @@ import './about.css'
 const About = () => {
   return (
     <section id='about'>
-    about
+      <div className='container'>
+                <h3>Nuestro proyecto se basa en la cooperacion mutua</h3>
+                <h1>Necesitas Presencia Web</h1>
+               <h4>En Argen-Develo, nos apasiona brindar apoyo a asociaciones sin fines de lucro y micro emprendimientos al ofrecerles páginas web de forma gratuita. Nuestro objetivo es ayudar a que estas organizaciones y negocios en crecimiento tengan una presencia en línea efectiva</h4> 
+                <p>¿Eres una asociación sin fines de lucro o un micro emprendimiento en busca de una presencia en línea? ¡Contáctanos hoy mismo para obtener tu página web gratuita y dar el siguiente paso hacia el éxito en línea!"</p>
+      </div>
     </section>
   )
 }
--- src/componets/experience/Experience.jsx
+++ src/componets/experience/Experience.jsx
@@ -4,7 +4,13 @@ import './experience.css'
 const Experience = () => {
   return (
     <section id='experience'>
-    Experience
+    <div className='container'>
+    <h3>Unete a Nuestro equipo</h3>
+    <h1>Nuestro proyecto se basa en las oportunidades</h1>
+  <p>En Argen-Develo, creemos en el poder de la colaboración y el aprendizaje conjunto. Ofrecemos una oportunidad única para que personas sin experiencia en en el ambito laboral se unan a nuestra red y adquieran habilidades valiosas mientras colaboran en emocionantes proyectos</p> 
+  <p>Nuestro proyecto está diseñado para ser una plataforma de aprendizaje en la que los participantes pueden adquirir experiencia práctica en el desarrollo web. A través de la colaboración con otros miembros de la red, cada persona tiene la oportunidad de aprender de diferentes perspectivas y enfoques</p>
+  
+  </div>
     </section>
   )
 }
