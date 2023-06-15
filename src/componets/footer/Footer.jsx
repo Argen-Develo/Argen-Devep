bbb456c2882401a3f437091e2197d90f7afcf04c
juanAriel <arielvia2013@gmail.com>
2023-06-15 14:21:17 -0400

    desarrollo de la seccion footer con estilos

--- src/componets/footer/Footer.jsx
+++ src/componets/footer/Footer.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import {CiLinkedin} from 'react-icons/ci'
+import {RxGithubLogo} from 'react-icons/rx'
+import {BsFacebook} from 'react-icons/bs'
 import './footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className='redes_sociales'>
+        <div className='socialmeida'>
+        <h4>Siguenos en...</h4>
+        <a href='https://linkedin.com' target='_blank' rel='noreferrer'><CiLinkedin/></a>
+        <a href='https://github.com' target='_blank' rel='noreferrer'><RxGithubLogo/></a>
+        <a href='https://Facebook.com' target='_blank' rel='noreferrer'><BsFacebook/></a>
+        </div>
+      </div>
+      <div className='info_copy'>
+        <div className='footer_copyringht'>
+          <p>
+            @{new Date().getFullYear()}Argen Develop. All rigth reserved.
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
